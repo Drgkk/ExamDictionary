5237b91b9491942ae36abdac9c98f83f60095914
Vadim Kharchencko <vadimlemetev@gmail.com>
2023-06-27 01:44:19 +0300

    Changes

--- ExamDictionary/Dictionary.cs
+++ ExamDictionary/Dictionary.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +28,15 @@ namespace ExamDictionary
             string? name = Console.ReadLine();
             if(name == "")
                 throw new Exception("Null Value!");
+
+            string[] q = (from w in words
+                          select w.MainWord).ToArray();
+
+            if(Array.IndexOf(q, name) != -1)
+            {
+                throw new Exception("Word already in dictionary!");
+            }
+
             Word word = new Word(name);
             Console.WriteLine("Write first definition of a word: ");
             name = Console.ReadLine();
@@ -48,12 +58,19 @@ namespace ExamDictionary
                             Console.WriteLine("Null value!");
                             continue;
                         }
+                        if (Array.IndexOf(word.Translations.ToArray(), name) != -1)
+                        {
+                            Console.WriteLine("Definition already exists!");
+                            continue;
+                        }
                         word.Translations.Add(name);
                         break;
                     case 1:
+                        words.Add(word);
                         AddWord();
                         break;
                     case 2:
+                        words.Add(word);
                         return;
                         break;
                 }
@@ -83,12 +100,19 @@ namespace ExamDictionary
                             Console.WriteLine("Null value!");
                             continue;
                         }
+                        if (Array.IndexOf(word.Translations.ToArray(), name) != -1)
+                        {
+                            Console.WriteLine("Definition already exists!");
+                            continue;
+                        }
                         word.Translations.Add(name);
                         break;
                     case 1:
+                        words.Add(word);
                         AddWord();
                         break;
                     case 2:
+                        words.Add(word);
                         return;
                         break;
                 }
@@ -106,6 +130,82 @@ namespace ExamDictionary
         public void ManipulateWord(string word)
         {
             string[] options = {"Delete word", "Add Definiton", "Delete Definition" };
+            int choice;
+            while(true)
+            {
+                try
+                {
+                    choice = Menu.ChooseItem(options, "Manipulate Word", 5, 5);
+                    switch (choice)
+                    {
+                        case 0:
+                            string[] q = (from w in words
+                                          select w.MainWord).ToArray();
+
+                            words.Remove(words[Array.IndexOf(q, word)]);
+                            break;
+                        case 1:
+                            string[] q1 = (from w in words
+                                           select w.MainWord).ToArray();
+
+                            AddDefinition(Array.IndexOf(q1, word));
+                            break;
+                        case 2:
+
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Error: {ex.Message}\nPress any key to continue");
+                    Console.ResetColor();
+                    Console.ReadKey(true);
+                }
+            }
+        }
+
+        private void AddDefinition(int index)
+        {
+            Console.WriteLine("Add definition of a word: ");
+            string name = Console.ReadLine();
+            if (name == "")
+                throw new Exception("Null Value!");
+
+            if (Array.IndexOf(words[index].Translations.ToArray(), name) != -1)
+            {
+                throw new Exception("Definition already exists!");
+            }
+            words[index].Translations.Add(name);
+
+            string[] options = { "Add another definition", "Finish adding definitions" };
+            int choice;
+            while (true)
+            {
+                choice = Menu.ChooseItem(options, "Adding Definitions...", 5, 5);
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("Write new definition of a word: ");
+                        name = Console.ReadLine();
+                        if (name == "")
+                        {
+                            Console.WriteLine("Null value!");
+                            continue;
+                        }
+                        if (Array.IndexOf(words[index].Translations.ToArray(), name) != -1)
+                        {
+                            Console.WriteLine("Definition already exists!");
+                            continue;
+                        }
+                        words[index].Translations.Add(name);
+                        break;
+                    case 2:
+                        return;
+                        break;
+                }
+
+            }
         }
     }
 }
